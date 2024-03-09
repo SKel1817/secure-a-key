f82e78c5e1d9b58d655c1220c73b97516604e20f
SKel1817 <vnielson@oakland.edu>
2024-03-09 13:54:11 -0500

    static folder for pages made

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/phising_mini_game")
+def phising_mini_game():
+    return render_template('phising_mini_game.html')
+
+@app.route('/emails.json')
+def emails():
+    return send_from_directory('static', 'emails.json')
 if __name__ == '__main__':
     app.run()
+
+
