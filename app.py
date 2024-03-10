dd0076a4be2bb4bc50ab8b2a30cffe9c5b31fcf0
SKel1817 <vnielson@oakland.edu>
2024-03-10 10:04:36 -0400

    password cracker and fixed links

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, send_from_directory
 from flask import request
 from waitress import serve
+import hashlib
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,7 +25,9 @@ def password_test():
     password = request.args.get('password', '')
     # Simulate cracking time based on password length and complexity
     cracking_time = simulate_cracking_time(password)
-    return render_template('passwordTest.html', password=password, cracking_time=cracking_time)
+    show_password = hash_password(password)
+    crack_pass = crack_the_password(password, show_password)
+    return render_template('passwordTest.html', password=password, cracking_time=cracking_time, show_password=show_password, crack_pass=crack_pass)
 
 def simulate_cracking_time(password):
     # This is a simplistic simulation; real cracking times depend on many factors
@@ -33,6 +36,22 @@ def simulate_cracking_time(password):
     time_to_crack = (length * complexity) / 2  # Simplified formula for demonstration
     return max(1, time_to_crack)  # Ensure at least 1 second is shown for simplicity
 
+
+def hash_password(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+
+def crack_the_password(u_password, hashed_password):
+    with open('passwords.txt', 'r') as f:
+        common_password = f.read().splitlines()
+    user_password = hashed_password
+    for password in common_password:
+        print("Trying password:", password)
+        master_hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        if master_hashed_password == user_password:
+            return "password found, it is " + u_password
+    return "password not found, Your password is safe!"   
+    
 @app.route('/twofactor')
 def twofactor():
     return render_template('twofactor.html')
